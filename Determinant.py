75166de312b85b644816a52b22bd417a6f51408f
Fatima-Shehzad <78612041+Fatima-Shehzad@users.noreply.github.com>
2021-02-05 22:07:56 +0500

    Add files via upload

---
+++ Determinant.py
@@ -0,0 +1,21 @@
+# According to user
+mat_1=[]
+rows = int(input("Enter the no of rows "))
+cols = int(input("Enter the no of columns "))
+for i in range(rows):
+    mat_1.append([0]*cols)
+
+print("Your matrix has now been created")
+for i in mat_1:
+    print(i)
+
+for i in range(len(mat_1)):
+    for j in range(len(mat_1[i])):
+        mat_1[i][j]+= int(input("Enter the number for position "+"["+str(i)+"] ["+str(j) +"] : "))
+
+
+for m in mat_1:
+    print(m)
+
+deter=( mat_1[0][0]* mat_1[1][1]) - (mat_1[0][1] * mat_1[1][0])
+print(deter)
